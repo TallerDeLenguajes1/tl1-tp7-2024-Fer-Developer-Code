138e869772541fa12ea60691983cb795ed9a575a
Fer <ibanez.fernando.2018@gmail.com>
2024-05-21 12:41:29 -0300

    limpiando

--- Calculadora.cs
+++ Calculadora.cs
@@ -2,6 +2,10 @@ namespace espacioCalculdora
 {
     public class Calculadora
     {
-        
+        void Sumar(double termino);
+        void Restar(double termino);
+        void Multiplicar(double termino);
+        void Dividir(double termino);
+        void limpiar();
     }
 }
