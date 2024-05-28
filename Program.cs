70ae3598ab42d39a63ff42fbf2d5a3eb7b23287c
Fernando <ibanez.fernando.2018@gmail.com>
2024-05-28 00:23:14 -0300

    Listo

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
 ﻿using espacioEmpleado;
-Empleado[] empleados = new Empleado[3];
-            // Aquí puedes agregar el código para cargar los datos de los empleados
 
-            double totalSalarios = 0;
-            foreach (Empleado empleado in empleados)
-            {
-                totalSalarios += empleado.SalarioFinal();
-            }
-            Console.WriteLine($"El monto total de salarios es: {totalSalarios}");
+// Inicializar el primer empleado
+Empleado empleado1 = new Empleado();
+empleado1.Nombre = "Juan";
+empleado1.Apellido = "Pérez";
+empleado1.FechaNacimiento = new DateTime(1980, 5, 1);
+empleado1.EstadoCivil = 'C';
+empleado1.FechaIngreso = new DateTime(2000, 1, 1);
+empleado1.Sueldo = 50000;
+empleado1.Posicion = Empleado.Cargo.Ingeniero;
 
-            Empleado masProximoAJubilarse = null;
-            foreach (Empleado empleado in empleados)
-            {
-                if (masProximoAJubilarse == null || empleado.EdadParaJubilarse() < masProximoAJubilarse.EdadParaJubilarse())
-                {
-                    masProximoAJubilarse = empleado;
-                }
-            }
-            Console.WriteLine($"El empleado más próximo a jubilarse es: {masProximoAJubilarse.Nombre} {masProximoAJubilarse.Apellido}");
+// Inicializar el segundo empleado
+Empleado empleado2 = new Empleado();
+empleado2.Nombre = "María";
+empleado2.Apellido = "González";
+empleado2.FechaNacimiento = new DateTime(1985, 6, 15);
+empleado2.EstadoCivil = 'S';
+empleado2.FechaIngreso = new DateTime(2005, 1, 1);
+empleado2.Sueldo = 45000;
+empleado2.Posicion = Empleado.Cargo.Administrativo;
+
+// Inicializar el tercer empleado
+Empleado empleado3 = new Empleado();
+empleado3.Nombre = "Carlos";
+empleado3.Apellido = "Rodríguez";
+empleado3.FechaNacimiento = new DateTime(1975, 7, 30);
+empleado3.EstadoCivil = 'C';
+empleado3.FechaIngreso = new DateTime(1995, 1, 1);
+empleado3.Sueldo = 55000;
+empleado3.Posicion = Empleado.Cargo.Especialista;
