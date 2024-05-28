4cee4ae31c2d1becf1e0a9386f831ff5c7d5e8d9
Fernando <ibanez.fernando.2018@gmail.com>
2024-05-28 00:18:53 -0300

    Terminado

--- Empleado.cs
+++ Empleado.cs
@@ -3,12 +3,21 @@ namespace espacioEmpleado
     public class Empleado
     {
         private string nombre;
-
         private string apellido;
         private DateTime fechaNacimiento;
         private char estadoCivil;
         private DateTime fechaIngreso;
         private double sueldo;
+        private Cargo posicion;
+
+        public enum Cargo
+        {
+            Auxiliar,
+            Administrativo,
+            Ingeniero,
+            Especialista,
+            Investigador
+        }
 
         public string Nombre { get => nombre; set => nombre = value; }
         public string Apellido { get => apellido; set => apellido = value; }
@@ -16,14 +25,35 @@ namespace espacioEmpleado
         public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
         public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
         public double Sueldo { get => sueldo; set => sueldo = value; }
+        public Cargo Posicion { get => posicion; set => posicion = value; }
 
-        private enum Cargo
+        public int Antiguedad()
         {
-            Auxiliar,
-            Administrativo,
-            Ingeniero,
-            Especialista,
-            Investigador
+            return DateTime.Now.Year - FechaIngreso.Year;
+        }
+
+        public int EdadEmpleado()
+        {
+            return DateTime.Now.Year - FechaNacimiento.Year;
+        }
+
+        public int EdadParaJubilarse()
+        {
+            return 65 - EdadEmpleado();
+        }
+
+        public double SalarioFinal()
+        {
+            double adicional = Sueldo * 0.01 * Math.Min(Antiguedad(), 20);
+            if (Posicion == Cargo.Ingeniero || Posicion == Cargo.Especialista)
+            {
+                adicional *= 1.5;
+            }
+            if (EstadoCivil == 'C')
+            {
+                adicional += 150000;
+            }
+            return Sueldo + adicional;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
 ﻿using espacioEmpleado;
+Empleado[] empleados = new Empleado[3];
+            // Aquí puedes agregar el código para cargar los datos de los empleados
+
+            double totalSalarios = 0;
+            foreach (Empleado empleado in empleados)
+            {
+                totalSalarios += empleado.SalarioFinal();
+            }
+            Console.WriteLine($"El monto total de salarios es: {totalSalarios}");
+
+            Empleado masProximoAJubilarse = null;
+            foreach (Empleado empleado in empleados)
+            {
+                if (masProximoAJubilarse == null || empleado.EdadParaJubilarse() < masProximoAJubilarse.EdadParaJubilarse())
+                {
+                    masProximoAJubilarse = empleado;
+                }
+            }
+            Console.WriteLine($"El empleado más próximo a jubilarse es: {masProximoAJubilarse.Nombre} {masProximoAJubilarse.Apellido}");
